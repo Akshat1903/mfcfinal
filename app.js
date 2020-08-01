8e9be198c36dee956c5dfb6efa13adf0091b80c2
Manav Nanwani <manav.nanwani@gmail.com>
2020-08-01 17:11:39 +0530

    arrows added

--- app.js
+++ app.js
@@ -123,6 +123,10 @@ var swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
       },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 
     });
 

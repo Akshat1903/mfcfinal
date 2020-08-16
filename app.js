05ec57e0c92b64b6a1558c292c58aeb2990acea6
Manav Nanwani <manav.nanwani@gmail.com>
2020-08-16 16:38:57 +0530

    minor

--- app.js
+++ app.js
@@ -77,3 +77,37 @@ function deletingEffect() {
 };
 
 typingEffect();
+
+
+var swiper = new Swiper('.swiper-container', {
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 30,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });
+        var swiper = new Swiper('.swiper_events', {
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 30,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });

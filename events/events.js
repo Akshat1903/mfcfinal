9f29e41fc990f1c45e5f563215c37503ed797a2b
Akshat Gupta <akshatgupta1903@gmail.com>
2020-08-20 12:59:13 +0530

    Navbar Added to Events Page

--- events/events.js
+++ events/events.js
@@ -1,3 +1,35 @@
+$(document).ready();
+$(function(){
+    $(document).scroll(function(){
+        var $nav = $("nav");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    })
+});
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = 'navLinkFade 0.5s ease forwards ' + (index / 7 + 0.3) + 's';
+            }
+            console.log(index / 7);
+        });
+
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+
+
 function splitScrollGravitas(){
     const controller = new ScrollMagic.Controller();
 

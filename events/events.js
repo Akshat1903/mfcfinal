f3f820a81d45ec9540e9790476f510ea0750e55b
Shubh Gupta <shubhngupta04@gmail.com>
2020-08-22 02:48:07 +0530

    event page fixed

--- events/events.js
+++ events/events.js
@@ -17,56 +17,56 @@ function splitScrollGravitas(){
     }
 }
 
-function showContentGravitas(){
-    var len = document.querySelectorAll('.category-slides-1').length;
-    var button;
-    for (var i=1;i<=len;i++){
-        document.getElementById("gravitas-"+i).addEventListener('click',function(e){
-            button = document.getElementById(e.target.id);
-            document.querySelector('.' + e.target.id).classList.toggle('hidden-content');
-            if (button.innerHTML === 'Know More'){
-                button.innerHTML = 'Show Less';
-            } else{
-                button.innerHTML = 'Know More';
-            }
-        });
-    }
-}
-
-function showContentRivera(){
-    var len = document.querySelectorAll('.category-slides-2').length;
-    var button;
-    for (var i=1;i<=len;i++){
-        document.getElementById("rivera-"+i).addEventListener('click',function(e){
-            button = document.getElementById(e.target.id);
-            document.querySelector('.' + e.target.id).classList.toggle('hidden-content');
-            if (button.innerHTML === 'Know More'){
-                button.innerHTML = 'Show Less';
-            } else{
-                button.innerHTML = 'Know More';
-            }
-        });
-    }
-}
-
-function showContentDaily(){
-    var len = document.querySelectorAll('.category-slides-3').length;
-    var button;
-    for (var i=1;i<=len;i++){
-        document.getElementById("daily-"+i).addEventListener('click',function(e){
-            button = document.getElementById(e.target.id);
-            document.querySelector('.' + e.target.id).classList.toggle('hidden-content');
-            if (button.innerHTML === 'Know More'){
-                button.innerHTML = 'Show Less';
-            } else{
-                button.innerHTML = 'Know More';
-            }
-        });
-    }
-}
+// function showContentGravitas(){
+//     var len = document.querySelectorAll('.category-slides-1').length;
+//     var button;
+//     for (var i=1;i<=len;i++){
+//         document.getElementById("gravitas-"+i).addEventListener('click',function(e){
+//             button = document.getElementById(e.target.id);
+//             document.querySelector('.' + e.target.id).classList.toggle('hidden-content');
+//             if (button.innerHTML === 'Know More'){
+//                 button.innerHTML = 'Show Less';
+//             } else{
+//                 button.innerHTML = 'Know More';
+//             }
+//         });
+//     }
+// }
+//
+// function showContentRivera(){
+//     var len = document.querySelectorAll('.category-slides-2').length;
+//     var button;
+//     for (var i=1;i<=len;i++){
+//         document.getElementById("rivera-"+i).addEventListener('click',function(e){
+//             button = document.getElementById(e.target.id);
+//             document.querySelector('.' + e.target.id).classList.toggle('hidden-content');
+//             if (button.innerHTML === 'Know More'){
+//                 button.innerHTML = 'Show Less';
+//             } else{
+//                 button.innerHTML = 'Know More';
+//             }
+//         });
+//     }
+// }
+//
+// function showContentDaily(){
+//     var len = document.querySelectorAll('.category-slides-3').length;
+//     var button;
+//     for (var i=1;i<=len;i++){
+//         document.getElementById("daily-"+i).addEventListener('click',function(e){
+//             button = document.getElementById(e.target.id);
+//             document.querySelector('.' + e.target.id).classList.toggle('hidden-content');
+//             if (button.innerHTML === 'Know More'){
+//                 button.innerHTML = 'Show Less';
+//             } else{
+//                 button.innerHTML = 'Know More';
+//             }
+//         });
+//     }
+// }
 
 
 splitScrollGravitas();
-showContentGravitas();
-showContentRivera();
-showContentDaily();
+// showContentGravitas();
+// showContentRivera();
+// showContentDaily();

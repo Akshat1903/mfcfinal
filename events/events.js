b77345f84a39720c04662bbadbb6867bed10c997
Akshat Gupta <akshatgupta1903@gmail.com>
2020-08-21 19:05:28 +0530

    Extra Code Removed

--- events/events.js
+++ events/events.js
@@ -1,34 +1,3 @@
-// $(document).ready();
-// $(function(){
-//     $(document).scroll(function(){
-//         var $nav = $("nav");
-//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-//     })
-// });
-
-
-// const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.nav-links');
-//     const navLinks = document.querySelectorAll('.nav-links li');
-//
-//     burger.addEventListener('click',()=>{
-//         nav.classList.toggle('nav-active');
-//
-//         navLinks.forEach((link, index) => {
-//             if(link.style.animation){
-//                 link.style.animation = '';
-//             }else{
-//                 link.style.animation = 'navLinkFade 0.5s ease forwards ' + (index / 7 + 0.3) + 's';
-//             }
-//             console.log(index / 7);
-//         });
-//
-//         burger.classList.toggle('toggle');
-//     });
-// }
-// navSlide();
-
 
 function splitScrollGravitas(){
     const controller = new ScrollMagic.Controller();
@@ -96,17 +65,7 @@ function showContentDaily(){
     }
 }
 
-// function setHeight(){
-//     for (var i=1;i<=3;i++){
-//         var len = document.querySelectorAll('.category-slides-'+i).length;
-//         var h = 0;
-//         for (var j=1;j<=len;j++){
-//
-//         }
-//         var height = (len*100) + 'vh';
-//         var percent = (len - 1)*100;
-//         document.querySelector('.about-' + i).style.height = height;
-// }
+
 splitScrollGravitas();
 showContentGravitas();
 showContentRivera();

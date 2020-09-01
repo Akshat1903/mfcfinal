ff4311afe3ecfc5cb68307dec2bf8ddc028123a7
Akshat Gupta <akshatgupta1903@gmail.com>
2020-09-01 12:48:14 +0530

    events.js fixed

--- events.js
+++ events.js
@@ -2,10 +2,11 @@
 function splitScrollGravitas(){
     const controller = new ScrollMagic.Controller();
 
-    for (var i=1;i<=4;i++){
+    for (var i=1;i<=3;i++){
         var len = document.querySelectorAll('.category-slides-'+i).length;
         var height = (len*100) + 'vh';
         var percent = (len - 1)*100;
+        console.log(percent);
         document.querySelector('.about-' + i).style.height = height;
         if(percent>0){
 
